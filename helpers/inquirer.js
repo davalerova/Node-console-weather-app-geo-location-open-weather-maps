fd778c5b2538ef55a1d27b5070b591740277eadf
David Valero <dvdovni@gmail.com>
2021-12-03 11:59:20 -0500

    Se crea el menú interactivo y el modelo para búsquedas

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -9,31 +9,15 @@ const preguntas = [
     message: 'Qué desea hacer?',
     choices: [
       {
-        value: '1',
-        name: `${'1.'.green} Crear tarea`,
+        value: 1,
+        name: `${'1.'.green} Buscar ciudad`,
       },
       {
-        value: '2',
-        name: `${'2.'.green} Listar tareas`,
+        value: 2,
+        name: `${'2.'.green} Historial`,
       },
       {
-        value: '3',
-        name: `${'3.'.green} Listar tareas completadas`,
-      },
-      {
-        value: '4',
-        name: `${'4.'.green} Listar tareas pendientes`,
-      },
-      {
-        value: '5',
-        name: `${'5.'.green} Completar tarea(s)`,
-      },
-      {
-        value: '6',
-        name: `${'6.'.green} Borrar tarea`,
-      },
-      {
-        value: '0',
+        value: 0,
         name: `${'0.'.green} Salir`,
       },
     ],
@@ -78,7 +62,7 @@ const leerInput = async (message) => {
   const { desc } = await inquirer.prompt(question);
   return desc;
 };
-
+/*
 const listadoTareasBorrar = async (tareas = []) => {
   const choices = tareas.map((tarea, i) => {
     const idx = `${i + 1}`.green;
@@ -131,13 +115,13 @@ const mostrarListadoChecklist = async (tareas = []) => {
   ];
   const { ids } = await inquirer.prompt(pregunta);
   return ids;
-};
+};*/
 
 module.exports = {
   inquirerMenu,
   pausa,
-  leerInput,
+  leerInput /*
   listadoTareasBorrar,
   confirmar,
-  mostrarListadoChecklist,
+  mostrarListadoChecklist,*/,
 };
--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const { leerInput } = require('./helpers/inquirer');
+const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
+require('colors');
 
 const main = async () => {
-  const texto = await leerInput('¿Nombre? ');
-  console.log(`Hola ${texto}`);
+  const busquedas = new Busquedas();
+  let opt;
+  do {
+    opt = await inquirerMenu();
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        const lugar = await leerInput('Ciudad: ');
+        console.log({ lugar });
+
+        // Buscar los lugares
+        // Seleccionar el lugar
+        // Clima
+        // Resultados
+        console.log('\nInformación de la ciudad\n'.green);
+        console.log('Cuidad: ');
+        console.log('Lat: ');
+        console.log('Lng: ');
+        console.log('Temperatura: ');
+        console.log('Mínima: ');
+        console.log('Máxima: ');
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+
+      default:
+        break;
+    }
+    opt !== 0 && (await pausa());
+  } while (opt !== 0);
 };
 
 main();
