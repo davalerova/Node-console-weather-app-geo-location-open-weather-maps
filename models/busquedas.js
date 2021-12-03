1329e99379d6a2574be3777f4ceb248356bbe6ff
David Valero <dvdovni@gmail.com>
2021-12-03 11:56:27 -0500

    Se crea el menú interactivo y el modelo para busquedas

---
+++ models/busquedas.js
@@ -0,0 +1,14 @@
+class Busquedas {
+  historial = ['Bogotá', 'Melgar', ''];
+
+  constructor() {
+    // TODO: leer DB si existe
+  }
+
+  async ciudad(lugar = '') {
+    // Perición http
+    console.log(lugar);
+  }
+}
+
+module.exports = Busquedas;
